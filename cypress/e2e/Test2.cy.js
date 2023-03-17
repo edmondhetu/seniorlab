660a8f70aa2b304c1f4b37bed2e0fe9367cfbd4c
Edmond Jr. Hetu <edmond.hetu@hrsdc-rhdcc.gc.ca>
2023-03-17 13:20:35 -0400

    fix test

--- cypress/e2e/Test2.cy.js
+++ cypress/e2e/Test2.cy.js
@@ -1,5 +1,4 @@
 it('verify text and links are returning 200 status', () => {
-    cy.url().should('include', 'https://www.google.com')
-
-    
-  })
+  cy.visit('https://www.google.com')
+  cy.url().should('include', 'https://www.google.com')
+})
