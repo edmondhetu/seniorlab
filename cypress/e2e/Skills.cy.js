6e9deee875f0ee9c1778d35ad69e5d0abd9f53a3
rahul.ghosh <rahul.ghosh@hrsdc-rhdcc.gc.ca>
2023-03-20 10:24:25 -0400

    test

---
+++ cypress/e2e/Skills.cy.js
@@ -0,0 +1,24 @@
+/// <reference types="cypress"/>
+
+it('Skills Search' , () => { 
+    
+    cy.visit ('https://www.canada.ca/en/services/jobs/training.html')
+    cy.get(':nth-child(3) > .h5 > a').click()
+    cy.get('.col-md-8 > .row > :nth-child(1)').click()
+    cy.get(':nth-child(1) > .h5 > a').click()
+    cy.get(':nth-child(3) > .panel > .panel-body > h3 > a').click()
+    cy.get('#category')
+    .select('Employer')
+    .should('have.value', 'Employer')
+    //cy.get('#skills')
+    //.select
+    cy.log('Select Skills')
+    cy.get(':nth-child(2) > .form-group > .hght > label').contains('Skills')
+    cy.get('#skills').find('option').should('have.length', 10)
+
+    const skills = ['Any', 'Adaptability', 'Collaboration', 'Communication', 'Creativity/Innovation', 'Digital', 'Numeracy', 'Problem Solving', 'Reading', 'Writing']
+    skills.forEach(skills=>{
+        cy.get('#skills').select(skills)
+    })
+
+   })
--- cypress/e2e/Test2.cy.js
+++ cypress/e2e/Test2.cy.js
@@ -1,4 +1,4 @@
 it('verify text and links are returning 200 status', () => {
-  cy.visit('https://www.google.com')
-  cy.url().should('include', 'https://www.google.com')
+  cy.visit('https://www.corel.com')
+  cy.url().should('include', 'https://www.corel.com')
 })
