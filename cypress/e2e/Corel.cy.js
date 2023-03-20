6dec3b97fff7ef189b16d1f532fe6c393598b51e
rahul.ghosh <rahul.ghosh@hrsdc-rhdcc.gc.ca>
2023-03-20 10:35:31 -0400

    Change Corel to Mitel

--- cypress/e2e/Corel.cy.js
+++ cypress/e2e/Corel.cy.js
@@ -1,4 +1,4 @@
 it('verify text and links are returning 200 status', () => {
-    cy.visit('https://www.corel.com')
+    cy.visit('https://www.Mitel.com')
     //cy.url().should('include', 'https://www.corel.com')
   })
