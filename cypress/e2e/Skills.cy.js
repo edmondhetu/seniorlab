6f1095e8c3a8dc7c0432de75871c6cd59c876bd8
Edmond Jr. Hetu <edmond.hetu@hrsdc-rhdcc.gc.ca>
2023-03-21 14:00:19 -0400

    format and insert comment

--- cypress/e2e/Skills.cy.js
+++ cypress/e2e/Skills.cy.js
@@ -1,15 +1,15 @@
 /// <reference types="cypress"/>
 
-it('Skills Search' , () => { 
-    
-    cy.visit ('https://www.canada.ca/en/services/jobs/training.html')
+it('Skills Search', () => {
+    //Please note this test fails on workflow pipeline. Thanks.
+    cy.visit('https://www.canada.ca/en/services/jobs/training.html')
     cy.get(':nth-child(3) > .h5 > a').click()
     cy.get('.col-md-8 > .row > :nth-child(1)').click()
     cy.get(':nth-child(1) > .h5 > a').click()
     cy.get(':nth-child(3) > .panel > .panel-body > h3 > a').click()
     cy.get('#category')
-    .select('Employer')
-    .should('have.value', 'Employer')
+        .select('Employer')
+        .should('have.value', 'Employer')
     //cy.get('#skills')
     //.select
     cy.log('Select Skills')
@@ -17,8 +17,8 @@ it('Skills Search' , () => {
     cy.get('#skills').find('option').should('have.length', 9)
 
     const skills = ['Any', 'Adaptability', 'Collaboration', 'Communication', 'Creativity/Innovation', 'Digital', 'Numeracy', 'Problem Solving', 'Reading', 'Writing']
-    skills.forEach(skills=>{
+    skills.forEach(skills => {
         cy.get('#skills').select(skills)
     })
 
-   })
+})
