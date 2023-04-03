760d620e1526cdfbb29f288e2d26d64ac7c41577
edmondhetu <edmondhetu88@gmail.com>
2023-04-03 10:36:19 -0400

    remove from json file screen size

--- cypress/e2e/Test207.spec.cy.js
+++ cypress/e2e/Test207.spec.cy.js
@@ -1,11 +1,12 @@
-import example from "/repos/seniorlab/cypress/fixtures/example.json"
+var sizes = ["iphone-3", "iphone-6", "ipad-mini", "macbook-13", "macbook-11", "macbook-16"]
+
 import { utility } from "../support/Utility"
 
 describe('test id 207 - Navigation with resolutions', () => {
-    context(`${example.sizes[0]} screen`, () => {
+    context(`${sizes[0]} screen`, () => {
         beforeEach(() => {
             cy.visit('/')
-            cy.viewport(example.sizes[0])
+            cy.viewport(sizes[0])
         })
 
         it('Government Benefits', () => {
@@ -30,10 +31,10 @@ describe('test id 207 - Navigation with resolutions', () => {
         })
     })
 
-    context(`${example.sizes[1]} screen`, () => {
+    context(`${sizes[1]} screen`, () => {
         beforeEach(() => {
             cy.visit('/')
-            cy.viewport(example.sizes[1])
+            cy.viewport(sizes[1])
         })
 
         it('Government Benefits', () => {
@@ -58,39 +59,10 @@ describe('test id 207 - Navigation with resolutions', () => {
         })
     })
 
-    context(`${example.sizes[2]} screen`, () => {
+    context(`${sizes[2]} screen`, () => {
         beforeEach(() => {
             cy.visit('/')
-            cy.viewport(example.sizes[2])
-        })
-
-        it('Government Benefits', () => {
-            cy.get('#english-button').click()
-            let language = new utility().getLanguageTabletOrMonitorScreen()
-            cy.log(language)
-            cy.get(':nth-child(1) > .max-h-0 > :nth-child(1) > .flex-col > .pt-5 > .py-2 > :nth-child(1) > .py-3 > .h4')
-                .should('have.text', language ? 'Government Benefits 101' : 'Government Benefits 101 (FR)')
-        })
-
-        it('Source of retirement income', () => {
-            cy.get('#english-button').click()
-            let language = new utility().getLanguageTabletOrMonitorScreen()
-            cy.get(':nth-child(1) > .max-h-0 > :nth-child(1) > .flex-col > .pt-5 > .py-2 > :nth-child(2) > .py-3 > .h4')
-                .should('have.text', language ? 'Sources of retirement income' : 'Sources of retirement income (FR)')
-        })
-
-        it('Financial Support', () => {
-            cy.get('#english-button').click()
-            let language = new utility().getLanguageTabletOrMonitorScreen()
-            cy.get(':nth-child(1) > .max-h-0 > :nth-child(1) > .flex-col > .pt-5 > .py-2 > :nth-child(3) > .py-3 > .h4')
-                .should('have.text', language ? 'Financial Support' : 'Financial Support (FR)')
-        })
-    })
-
-    context(`${example.sizes[3]} screen`, () => {
-        beforeEach(() => {
-            cy.visit('/')
-            cy.viewport(example.sizes[3])
+            cy.viewport(sizes[2])
         })
 
         it('Government Benefits', () => {
@@ -116,10 +88,39 @@ describe('test id 207 - Navigation with resolutions', () => {
         })
     })
 
-    context(`${example.sizes[4]} screen`, () => {
+    context(`${sizes[3]} screen`, () => {
         beforeEach(() => {
             cy.visit('/')
-            cy.viewport(example.sizes[4])
+            cy.viewport(sizes[3])
+        })
+
+        it('Government Benefits', () => {
+            cy.get('#english-button').click()
+            let language = new utility().getLanguageTabletOrMonitorScreen()
+            cy.log(language)
+            cy.get(':nth-child(1) > .max-h-0 > :nth-child(1) > .flex-col > .pt-5 > .py-2 > :nth-child(1) > .py-3 > .h4')
+                .should('have.text', language ? 'Government Benefits 101' : 'Government Benefits 101 (FR)')
+        })
+
+        it('Source of retirement income', () => {
+            cy.get('#english-button').click()
+            let language = new utility().getLanguageTabletOrMonitorScreen()
+            cy.get(':nth-child(1) > .max-h-0 > :nth-child(1) > .flex-col > .pt-5 > .py-2 > :nth-child(2) > .py-3 > .h4')
+                .should('have.text', language ? 'Sources of retirement income' : 'Sources of retirement income (FR)')
+        })
+
+        it('Financial Support', () => {
+            cy.get('#english-button').click()
+            let language = new utility().getLanguageTabletOrMonitorScreen()
+            cy.get(':nth-child(1) > .max-h-0 > :nth-child(1) > .flex-col > .pt-5 > .py-2 > :nth-child(3) > .py-3 > .h4')
+                .should('have.text', language ? 'Financial Support' : 'Financial Support (FR)')
+        })
+    })
+
+    context(`${sizes[4]} screen`, () => {
+        beforeEach(() => {
+            cy.visit('/')
+            cy.viewport(sizes[4])
         })
 
         it('Government Benefits', () => {
@@ -144,10 +145,10 @@ describe('test id 207 - Navigation with resolutions', () => {
         })
     })
 
-    context(`${example.sizes[5]} screen`, () => {
+    context(`${sizes[5]} screen`, () => {
         beforeEach(() => {
             cy.visit('/')
-            cy.viewport(example.sizes[5])
+            cy.viewport(sizes[5])
         })
 
         it('Government Benefits', () => {
