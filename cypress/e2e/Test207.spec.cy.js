a32b05bac732c38a930e9f0425385046992ea0c6
Edmond Jr. Hetu <edmond.hetu@hrsdc-rhdcc.gc.ca>
2023-04-03 14:17:55 -0400

    fix test 207

--- cypress/e2e/Test207.spec.cy.js
+++ cypress/e2e/Test207.spec.cy.js
@@ -1,32 +1,41 @@
 var sizes = ["iphone-3", "iphone-6", "ipad-mini", "macbook-13", "macbook-11", "macbook-16"]
 import { utility } from "../support/Utility"
 
-describe('test id 207 - Navigation with resolutions', () => {
+describe.only('test id 207 - Navigation with resolutions', () => {
     context(`${sizes[0]} screen`, () => {
         beforeEach(() => {
             cy.visit('/')
             cy.viewport(sizes[0])
         })
 
-        it('Government Benefits', () => {
+        it('Government Benefits link title and paragraph content', () => {
             cy.get('#english-button').click()
             let language = new utility().getLanguageMobileScreen()
             cy.get(':nth-child(1) > .max-h-0 > :nth-child(1) > .flex-col > .pt-5 > .py-2 > :nth-child(1) > .py-3 > .h4')
                 .should('have.text', language ? 'Government Benefits 101' : 'Government Benefits 101 (FR)')
+
+            cy.get(':nth-child(1) > .max-h-0 > :nth-child(1) > .flex-col > .pt-5 > .py-2 > :nth-child(1) > :nth-child(2)')
+                .should('have.text', language ? 'Learn about CPP/QPP and OAS' : '(FR) Learn about CPP/QPP and OAS')
         })
 
-        it('Source of retirement income', () => {
+        it('Source of retirement income link title and paragraph content', () => {
             cy.get('#english-button').click()
             let language = new utility().getLanguageMobileScreen()
             cy.get(':nth-child(1) > .max-h-0 > :nth-child(1) > .flex-col > .pt-5 > .py-2 > :nth-child(2) > .py-3 > .h4')
                 .should('have.text', language ? 'Sources of retirement income' : 'Sources of retirement income (FR)')
+
+            cy.get(':nth-child(1) > .max-h-0 > :nth-child(1) > .flex-col > .pt-5 > .py-2 > :nth-child(2) > :nth-child(2)')
+                .should('have.text', language ? 'Your income during retirement will typically come from three main sources' : '(FR) Your income during retirement will typically come from three main sources')
         })
 
-        it('Financial Support', () => {
+        it('Financial Support link title and paragraph content', () => {
             cy.get('#english-button').click()
             let language = new utility().getLanguageMobileScreen()
             cy.get(':nth-child(1) > .max-h-0 > :nth-child(1) > .flex-col > .pt-5 > .py-2 > :nth-child(3) > .py-3 > .h4')
                 .should('have.text', language ? 'Financial Support' : 'Financial Support (FR)')
+
+            cy.get(':nth-child(1) > .max-h-0 > :nth-child(1) > .flex-col > .pt-5 > .py-2 > :nth-child(3) > :nth-child(2)')
+                .should('have.text', language ? 'Learn about your different financial support options.' : '(FR) Learn about your different financial support options.')
         })
     })
 
@@ -36,25 +45,34 @@ describe('test id 207 - Navigation with resolutions', () => {
             cy.viewport(sizes[1])
         })
 
-        it('Government Benefits', () => {
+        it('Government Benefits link title and paragraph content', () => {
             cy.get('#english-button').click()
             let language = new utility().getLanguageMobileScreen()
             cy.get(':nth-child(1) > .max-h-0 > :nth-child(1) > .flex-col > .pt-5 > .py-2 > :nth-child(1) > .py-3 > .h4')
                 .should('have.text', language ? 'Government Benefits 101' : 'Government Benefits 101 (FR)')
+
+            cy.get(':nth-child(1) > .max-h-0 > :nth-child(1) > .flex-col > .pt-5 > .py-2 > :nth-child(1) > :nth-child(2)')
+                .should('have.text', language ? 'Learn about CPP/QPP and OAS' : '(FR) Learn about CPP/QPP and OAS')
         })
 
-        it('Source of retirement income', () => {
+        it('Source of retirement income link title and paragraph content', () => {
             cy.get('#english-button').click()
             let language = new utility().getLanguageMobileScreen()
             cy.get(':nth-child(1) > .max-h-0 > :nth-child(1) > .flex-col > .pt-5 > .py-2 > :nth-child(2) > .py-3 > .h4')
                 .should('have.text', language ? 'Sources of retirement income' : 'Sources of retirement income (FR)')
+
+            cy.get(':nth-child(1) > .max-h-0 > :nth-child(1) > .flex-col > .pt-5 > .py-2 > :nth-child(2) > :nth-child(2)')
+                .should('have.text', language ? 'Your income during retirement will typically come from three main sources' : '(FR) Your income during retirement will typically come from three main sources')
         })
 
-        it('Financial Support', () => {
+        it('Financial Support link title and paragraph content', () => {
             cy.get('#english-button').click()
             let language = new utility().getLanguageMobileScreen()
             cy.get(':nth-child(1) > .max-h-0 > :nth-child(1) > .flex-col > .pt-5 > .py-2 > :nth-child(3) > .py-3 > .h4')
                 .should('have.text', language ? 'Financial Support' : 'Financial Support (FR)')
+
+            cy.get(':nth-child(1) > .max-h-0 > :nth-child(1) > .flex-col > .pt-5 > .py-2 > :nth-child(3) > :nth-child(2)')
+                .should('have.text', language ? 'Learn about your different financial support options.' : '(FR) Learn about your different financial support options.')
         })
     })
 
@@ -64,26 +82,34 @@ describe('test id 207 - Navigation with resolutions', () => {
             cy.viewport(sizes[2])
         })
 
-        it('Government Benefits', () => {
+        it('Government Benefits link title and paragraph content', () => {
             cy.get('#english-button').click()
             let language = new utility().getLanguageTabletOrMonitorScreen()
-            cy.log(language)
             cy.get(':nth-child(1) > .max-h-0 > :nth-child(1) > .flex-col > .pt-5 > .py-2 > :nth-child(1) > .py-3 > .h4')
                 .should('have.text', language ? 'Government Benefits 101' : 'Government Benefits 101 (FR)')
+
+            cy.get(':nth-child(1) > .max-h-0 > :nth-child(1) > .flex-col > .pt-5 > .py-2 > :nth-child(1) > :nth-child(2)')
+                .should('have.text', language ? 'Learn about CPP/QPP and OAS' : '(FR) Learn about CPP/QPP and OAS')
         })
 
-        it('Source of retirement income', () => {
+        it('Source of retirement income link title and paragraph content', () => {
             cy.get('#english-button').click()
             let language = new utility().getLanguageTabletOrMonitorScreen()
             cy.get(':nth-child(1) > .max-h-0 > :nth-child(1) > .flex-col > .pt-5 > .py-2 > :nth-child(2) > .py-3 > .h4')
                 .should('have.text', language ? 'Sources of retirement income' : 'Sources of retirement income (FR)')
+
+            cy.get(':nth-child(1) > .max-h-0 > :nth-child(1) > .flex-col > .pt-5 > .py-2 > :nth-child(2) > :nth-child(2)')
+                .should('have.text', language ? 'Your income during retirement will typically come from three main sources' : '(FR) Your income during retirement will typically come from three main sources')
         })
 
-        it('Financial Support', () => {
+        it('Financial Support link title and paragraph content', () => {
             cy.get('#english-button').click()
             let language = new utility().getLanguageTabletOrMonitorScreen()
             cy.get(':nth-child(1) > .max-h-0 > :nth-child(1) > .flex-col > .pt-5 > .py-2 > :nth-child(3) > .py-3 > .h4')
                 .should('have.text', language ? 'Financial Support' : 'Financial Support (FR)')
+
+            cy.get(':nth-child(1) > .max-h-0 > :nth-child(1) > .flex-col > .pt-5 > .py-2 > :nth-child(3) > :nth-child(2)')
+                .should('have.text', language ? 'Learn about your different financial support options.' : '(FR) Learn about your different financial support options.')
         })
     })
 
@@ -93,26 +119,34 @@ describe('test id 207 - Navigation with resolutions', () => {
             cy.viewport(sizes[3])
         })
 
-        it('Government Benefits', () => {
+        it('Government Benefits link title and paragraph content', () => {
             cy.get('#english-button').click()
             let language = new utility().getLanguageTabletOrMonitorScreen()
-            cy.log(language)
             cy.get(':nth-child(1) > .max-h-0 > :nth-child(1) > .flex-col > .pt-5 > .py-2 > :nth-child(1) > .py-3 > .h4')
                 .should('have.text', language ? 'Government Benefits 101' : 'Government Benefits 101 (FR)')
+
+            cy.get(':nth-child(1) > .max-h-0 > :nth-child(1) > .flex-col > .pt-5 > .py-2 > :nth-child(1) > :nth-child(2)')
+                .should('have.text', language ? 'Learn about CPP/QPP and OAS' : '(FR) Learn about CPP/QPP and OAS')
         })
 
-        it('Source of retirement income', () => {
+        it('Source of retirement income link title and paragraph content', () => {
             cy.get('#english-button').click()
             let language = new utility().getLanguageTabletOrMonitorScreen()
             cy.get(':nth-child(1) > .max-h-0 > :nth-child(1) > .flex-col > .pt-5 > .py-2 > :nth-child(2) > .py-3 > .h4')
                 .should('have.text', language ? 'Sources of retirement income' : 'Sources of retirement income (FR)')
+
+            cy.get(':nth-child(1) > .max-h-0 > :nth-child(1) > .flex-col > .pt-5 > .py-2 > :nth-child(2) > :nth-child(2)')
+                .should('have.text', language ? 'Your income during retirement will typically come from three main sources' : '(FR) Your income during retirement will typically come from three main sources')
         })
 
-        it('Financial Support', () => {
+        it('Financial Support link title and paragraph content', () => {
             cy.get('#english-button').click()
             let language = new utility().getLanguageTabletOrMonitorScreen()
             cy.get(':nth-child(1) > .max-h-0 > :nth-child(1) > .flex-col > .pt-5 > .py-2 > :nth-child(3) > .py-3 > .h4')
                 .should('have.text', language ? 'Financial Support' : 'Financial Support (FR)')
+
+            cy.get(':nth-child(1) > .max-h-0 > :nth-child(1) > .flex-col > .pt-5 > .py-2 > :nth-child(3) > :nth-child(2)')
+                .should('have.text', language ? 'Learn about your different financial support options.' : '(FR) Learn about your different financial support options.')
         })
     })
 
@@ -122,25 +156,34 @@ describe('test id 207 - Navigation with resolutions', () => {
             cy.viewport(sizes[4])
         })
 
-        it('Government Benefits', () => {
+        it('Government Benefits link title and paragraph content', () => {
             cy.get('#english-button').click()
             let language = new utility().getLanguageTabletOrMonitorScreen()
             cy.get(':nth-child(1) > .max-h-0 > :nth-child(1) > .flex-col > .pt-5 > .py-2 > :nth-child(1) > .py-3 > .h4')
                 .should('have.text', language ? 'Government Benefits 101' : 'Government Benefits 101 (FR)')
+
+            cy.get(':nth-child(1) > .max-h-0 > :nth-child(1) > .flex-col > .pt-5 > .py-2 > :nth-child(1) > :nth-child(2)')
+                .should('have.text', language ? 'Learn about CPP/QPP and OAS' : '(FR) Learn about CPP/QPP and OAS')
         })
 
-        it('Source of retirement income', () => {
+        it('Source of retirement income link title and paragraph content', () => {
             cy.get('#english-button').click()
             let language = new utility().getLanguageTabletOrMonitorScreen()
             cy.get(':nth-child(1) > .max-h-0 > :nth-child(1) > .flex-col > .pt-5 > .py-2 > :nth-child(2) > .py-3 > .h4')
                 .should('have.text', language ? 'Sources of retirement income' : 'Sources of retirement income (FR)')
+
+            cy.get(':nth-child(1) > .max-h-0 > :nth-child(1) > .flex-col > .pt-5 > .py-2 > :nth-child(2) > :nth-child(2)')
+                .should('have.text', language ? 'Your income during retirement will typically come from three main sources' : '(FR) Your income during retirement will typically come from three main sources')
         })
 
-        it('Financial Support', () => {
+        it('Financial Support link title and paragraph content', () => {
             cy.get('#english-button').click()
             let language = new utility().getLanguageTabletOrMonitorScreen()
             cy.get(':nth-child(1) > .max-h-0 > :nth-child(1) > .flex-col > .pt-5 > .py-2 > :nth-child(3) > .py-3 > .h4')
                 .should('have.text', language ? 'Financial Support' : 'Financial Support (FR)')
+
+            cy.get(':nth-child(1) > .max-h-0 > :nth-child(1) > .flex-col > .pt-5 > .py-2 > :nth-child(3) > :nth-child(2)')
+                .should('have.text', language ? 'Learn about your different financial support options.' : '(FR) Learn about your different financial support options.')
         })
     })
 
@@ -150,25 +193,34 @@ describe('test id 207 - Navigation with resolutions', () => {
             cy.viewport(sizes[5])
         })
 
-        it('Government Benefits', () => {
+        it('Government Benefits link title and paragraph content', () => {
             cy.get('#english-button').click()
             let language = new utility().getLanguageTabletOrMonitorScreen()
             cy.get(':nth-child(1) > .max-h-0 > :nth-child(1) > .flex-col > .pt-5 > .py-2 > :nth-child(1) > .py-3 > .h4')
                 .should('have.text', language ? 'Government Benefits 101' : 'Government Benefits 101 (FR)')
+
+            cy.get(':nth-child(1) > .max-h-0 > :nth-child(1) > .flex-col > .pt-5 > .py-2 > :nth-child(1) > :nth-child(2)')
+                .should('have.text', language ? 'Learn about CPP/QPP and OAS' : '(FR) Learn about CPP/QPP and OAS')
         })
 
-        it('Source of retirement income', () => {
+        it('Source of retirement income link title and paragraph content', () => {
             cy.get('#english-button').click()
             let language = new utility().getLanguageTabletOrMonitorScreen()
             cy.get(':nth-child(1) > .max-h-0 > :nth-child(1) > .flex-col > .pt-5 > .py-2 > :nth-child(2) > .py-3 > .h4')
                 .should('have.text', language ? 'Sources of retirement income' : 'Sources of retirement income (FR)')
+
+            cy.get(':nth-child(1) > .max-h-0 > :nth-child(1) > .flex-col > .pt-5 > .py-2 > :nth-child(2) > :nth-child(2)')
+                .should('have.text', language ? 'Your income during retirement will typically come from three main sources' : '(FR) Your income during retirement will typically come from three main sources')
         })
 
-        it('Financial Support', () => {
+        it('Financial Support link title and paragraph content', () => {
             cy.get('#english-button').click()
             let language = new utility().getLanguageTabletOrMonitorScreen()
             cy.get(':nth-child(1) > .max-h-0 > :nth-child(1) > .flex-col > .pt-5 > .py-2 > :nth-child(3) > .py-3 > .h4')
                 .should('have.text', language ? 'Financial Support' : 'Financial Support (FR)')
+
+            cy.get(':nth-child(1) > .max-h-0 > :nth-child(1) > .flex-col > .pt-5 > .py-2 > :nth-child(3) > :nth-child(2)')
+                .should('have.text', language ? 'Learn about your different financial support options.' : '(FR) Learn about your different financial support options.')
         })
     })
 })
