0b079e94edc7ab74d88af45cbfe3258bc7143904
edmondhetu <edmondhetu88@gmail.com>
2023-04-11 14:00:40 -0400

    insert test162

--- cypress/e2e/Test213.spec.cy.js
+++ cypress/e2e/Test213.spec.cy.js
@@ -11,52 +11,52 @@ describe('test id 213 - Navigation with resolutions', () => {
         it('Learn', () => {
             cy.get('#english-button').click()
             let language = new utility().getLanguageMobileScreen()
-            cy.get(':nth-child(1) > .my-3 > .h4').click({ force: true })           
+            cy.get(':nth-child(1) > .my-3 > .h4').click({ force: true })
             cy.contains(language ? 'Learn' : 'Learn (FR)')
 
-            cy.get('.max-h-full > :nth-child(1) > .flex-col > .px-10 > .justify-center > :nth-child(1)')    
-            .should('have.text', language ? 'Learn about the "three pillars" of Canada\'s retirement system.' : '(FR) Learn about the "three pillars" of Canada\'s retirement system.')
+            cy.get('.max-h-full > :nth-child(1) > .flex-col > .px-10 > .justify-center > :nth-child(1)')
+                .should('have.text', language ? 'Learn about the "three pillars" of Canada\'s retirement system.' : '(FR) Learn about the "three pillars" of Canada\'s retirement system.')
             cy.get('.max-h-full > :nth-child(1) > .flex-col > .px-10 > .justify-center > :nth-child(2)')
-            .should('have.text', language ? 'There are many factors to consider before retiring to help ensure that you can comfortably retire.' : '(FR) There are many factors to consider before retiring to help ensure that you can comfortably retire.')
-      
+                .should('have.text', language ? 'There are many factors to consider before retiring to help ensure that you can comfortably retire.' : '(FR) There are many factors to consider before retiring to help ensure that you can comfortably retire.')
+
         })
 
         it('Plan', () => {
             cy.get('#english-button').click()
             let language = new utility().getLanguageMobileScreen()
-            cy.get(':nth-child(2) > .my-3 > .h4').click({ force: true })            
+            cy.get(':nth-child(2) > .my-3 > .h4').click({ force: true })
             cy.contains(language ? 'Plan' : 'Plan (FR)')
 
-            cy.get('.max-h-full > :nth-child(1) > .flex-col > .px-10 > .justify-center > :nth-child(1)')    
-            .should('have.text', language ? 'Learn about the "three pillars" of Canada\'s retirement system.' : '(FR) Learn about the "three pillars" of Canada\'s retirement system.')
+            cy.get('.max-h-full > :nth-child(1) > .flex-col > .px-10 > .justify-center > :nth-child(1)')
+                .should('have.text', language ? 'Learn about the "three pillars" of Canada\'s retirement system.' : '(FR) Learn about the "three pillars" of Canada\'s retirement system.')
             cy.get('.max-h-full > :nth-child(1) > .flex-col > .px-10 > .justify-center > :nth-child(2)')
-            .should('have.text', language ? 'There are many factors to consider before retiring to help ensure that you can comfortably retire.' : '(FR) There are many factors to consider before retiring to help ensure that you can comfortably retire.')
+                .should('have.text', language ? 'There are many factors to consider before retiring to help ensure that you can comfortably retire.' : '(FR) There are many factors to consider before retiring to help ensure that you can comfortably retire.')
         })
 
-            it('Apply', () => {
-                cy.get('#english-button').click()
-                let language = new utility().getLanguageMobileScreen()
-                cy.get(':nth-child(3) > .my-3 > .h4').click({ force: true })                
-                cy.contains(language ? 'Apply' : 'Apply (FR)')
-    
-                cy.get('.max-h-full > :nth-child(1) > .flex-col > .px-10 > .justify-center > :nth-child(1)')    
+        it('Apply', () => {
+            cy.get('#english-button').click()
+            let language = new utility().getLanguageMobileScreen()
+            cy.get(':nth-child(3) > .my-3 > .h4').click({ force: true })
+            cy.contains(language ? 'Apply' : 'Apply (FR)')
+
+            cy.get('.max-h-full > :nth-child(1) > .flex-col > .px-10 > .justify-center > :nth-child(1)')
                 .should('have.text', language ? 'Learn about the "three pillars" of Canada\'s retirement system.' : '(FR) Learn about the "three pillars" of Canada\'s retirement system.')
-                cy.get('.max-h-full > :nth-child(1) > .flex-col > .px-10 > .justify-center > :nth-child(2)')
+            cy.get('.max-h-full > :nth-child(1) > .flex-col > .px-10 > .justify-center > :nth-child(2)')
                 .should('have.text', language ? 'There are many factors to consider before retiring to help ensure that you can comfortably retire.' : '(FR) There are many factors to consider before retiring to help ensure that you can comfortably retire.')
-            })
-       
-            it('Manage', () => {
-                cy.get('#english-button').click()
-                let language = new utility().getLanguageMobileScreen()
-                cy.get(':nth-child(4) > .my-3 > .h4').click({ force: true })                
-                cy.contains(language ? 'Manage' : 'Manage (FR)')
-        
-                cy.get('.max-h-full > :nth-child(1) > .flex-col > .px-10 > .justify-center > :nth-child(1)')    
-                .should('have.text', language ? 'Learn about the "three pillars" of Canada\'s retirement system.' : '(FR) Learn about the "three pillars" of Canada\'s retirement system.')
-                cy.get('.max-h-full > :nth-child(1) > .flex-col > .px-10 > .justify-center > :nth-child(2)')
-                .should('have.text', language ? 'There are many factors to consider before retiring to help ensure that you can comfortably retire.' : '(FR) There are many factors to consider before retiring to help ensure that you can comfortably retire.')
-           
-            })
-      
         })
+
+        it('Manage', () => {
+            cy.get('#english-button').click()
+            let language = new utility().getLanguageMobileScreen()
+            cy.get(':nth-child(4) > .my-3 > .h4').click({ force: true })
+            cy.contains(language ? 'Manage' : 'Manage (FR)')
+
+            cy.get('.max-h-full > :nth-child(1) > .flex-col > .px-10 > .justify-center > :nth-child(1)')
+                .should('have.text', language ? 'Learn about the "three pillars" of Canada\'s retirement system.' : '(FR) Learn about the "three pillars" of Canada\'s retirement system.')
+            cy.get('.max-h-full > :nth-child(1) > .flex-col > .px-10 > .justify-center > :nth-child(2)')
+                .should('have.text', language ? 'There are many factors to consider before retiring to help ensure that you can comfortably retire.' : '(FR) There are many factors to consider before retiring to help ensure that you can comfortably retire.')
+
+        })
+
     })
+})
