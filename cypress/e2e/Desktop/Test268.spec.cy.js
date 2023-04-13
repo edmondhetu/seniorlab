0ab44dd80e97d792c4d0b384e5fc06d94c351c06
Edmond Jr. Hetu <edmond.hetu@hrsdc-rhdcc.gc.ca>
2023-04-13 11:46:16 -0400

    link text content

--- cypress/e2e/Desktop/Test268.spec.cy.js
+++ cypress/e2e/Desktop/Test268.spec.cy.js
@@ -1,7 +1,8 @@
 describe('test id 268 - verify Learn Overview - Breadcrumb', () => {
-  it.only('canada link click url redirects', () => {
+  it.only('canada link - verify link text and click url redirects', () => {
     cy.visit('/')
     cy.get('#english-button').click()
+    cy.get('.inline-block > .font-body').should('have.text', 'Canada.ca')
     cy.wait(2000)
     cy.get('.inline-block > .font-body').click()
     cy.origin('https://www.canada.ca', () => {
