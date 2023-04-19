4984e732469ab4d455cc7e9985546d84aa90ad69
edmondhetu <edmondhetu88@gmail.com>
2023-04-19 13:43:13 -0400

    fix test157

--- cypress/e2e/Desktop/Test157.spec.cy.js
+++ cypress/e2e/Desktop/Test157.spec.cy.js
@@ -7,27 +7,28 @@ describe('test id 157 - verify Supporting Seniors - Landing page ', () => {
   it('family and friends link and paragraph content', () => {
     cy.get('#english-button').click()
     let language = new utility().getLanguageTabletOrMonitorScreen()
-    cy.get('.grid > :nth-child(1) > a')
-      .should('have.text', language ? 'Family and Friends' : 'Family and Friends')
-    cy.get('.grid > :nth-child(1) > p')
-      .should('have.text', language ? 'Learn how you can help your loved ones enter the retirement stage in their life.' : 'FR Learn how you can help your loved ones enter the retirement stage in their life.')
+
+    cy.contains(language ? 'Family and FriendsLearn how you can help your loved ones enter the retirement stage in their life.' :
+      'FR Family and FriendsLearn how you can help your loved ones enter the retirement stage in their life.')
+      .should('have.text', language ? 'Family and FriendsLearn how you can help your loved ones enter the retirement stage in their life.' :
+        'FR Family and FriendsLearn how you can help your loved ones enter the retirement stage in their life.')
   })
 
   it('representatives and paragraph content', () => {
     cy.get('#english-button').click()
     let language = new utility().getLanguageTabletOrMonitorScreen()
-    cy.get('.grid > :nth-child(2) > a')
-    .should('have.text', 'Representatives')
-    cy.get('.grid > :nth-child(2) > p')
-      .should('have.text', language ? 'Find resources to understand and navigate Canada\'s pension system.' : 'FR Find resources to understand and navigate Canada’s pension system.')
+    cy.contains(language ? 'RepresentativesFind resources to understand and navigate Canada\'s pension system.' :
+      'FR RepresentativesFind resources to understand and navigate Canada\'s pension system.')
+      .should('have.text', language ? 'RepresentativesFind resources to understand and navigate Canada\'s pension system.' :
+        'FR RepresentativesFind resources to understand and navigate Canada\'s pension system.')
   })
 
   it('organization and paragraph content', () => {
     cy.get('#english-button').click()
     let language = new utility().getLanguageTabletOrMonitorScreen()
-    cy.get('.grid > :nth-child(3) > a')
-      .should('have.text', 'Organizations')
-    cy.get('.grid > :nth-child(3) > p')
-      .should('have.text', language ? 'Learn how best to help and navigate clients as they plan for retirement.' : 'FR Learn how best to help and navigate clients as they plan for retirement.')
+    cy.contains(language ? 'OrganizationsLearn how best to help and navigate clients as they plan for retirement.' :
+      'FR OrganizationsLearn how best to help and navigate clients as they plan for retirement.')
+      .should('have.text', language ? 'OrganizationsLearn how best to help and navigate clients as they plan for retirement.' :
+        'FR OrganizationsLearn how best to help and navigate clients as they plan for retirement.')
   })
 })
