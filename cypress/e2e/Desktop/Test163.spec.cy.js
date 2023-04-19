df07f8fa7a90756c2fe1992201f10a33b3909829
edmondhetu <edmondhetu88@gmail.com>
2023-04-19 14:13:10 -0400

    fix test 162 163

--- cypress/e2e/Desktop/Test163.spec.cy.js
+++ cypress/e2e/Desktop/Test163.spec.cy.js
@@ -5,32 +5,35 @@ describe('test id 163 - verify Learn overview - Explore our Content - Manage Mon
         cy.visit('/en/learn')
     })
     it('Learn overview - Explore our Content - Manage Money in retirement content', () => {
-        //cy.get('#english-button').click()
         let language = new utility().getLanguageTabletOrMonitorScreen()
-        cy.contains(language ? 'Explore our content' : '(FR) Explore our content')
         cy.contains(language ? 'Managing your money in retirement' : '(FR) Managing your money in retirement')
-        cy.contains(language ? 'Prepare for a comfortable retirement by exploring our money management section, which includes guidance on personal savings, identifying potential income sources, tax implications, and more - all with CPP and OAS benefits in mind.' :
-            '(FR) Prepare for a comfortable retirement by exploring our money management section, which includes guidance on personal savings, identifying potential income sources, tax implications, and more - all with CPP and OAS benefits in mind.')
+        cy.contains(language ? 'Prepare for old age by exploring retirement income sources and learn how to manage your money after you retire.' :
+            '(FR) Prepare for old age by exploring retirement income sources and learn how to manage your money after you retire.')
     })
 
     it('Learn overview - card 1 content', () => {
-        //cy.get('#english-button').click()
         let language = new utility().getLanguageTabletOrMonitorScreen()
-        cy.get(':nth-child(2) > .grid > :nth-child(1) > .no-underline > .p-6 > .text-xs').should('have.text',language ? '5 MIN READ' : '(FR) 5 MIN READ')
-        cy.get('#section-0-card-0').should('have.text',language ? 'Overview of retirement income sources' : '(FR) Overview of retirement income sources')
-        cy.get(':nth-child(2) > .grid > :nth-child(1) > .no-underline > .p-6 > .text-base').should('have.text', language ? 'Learn about public & private pensions, personal savings and investments.' : '(FR) Learn about public & private pensions, personal savings and investments.')
+        cy.get(':nth-child(1) > .grid > :nth-child(1) > .MuiButtonBase-root > .MuiCardContent-root > .text-sm')
+            .should('have.text', language ? '9 min read' : '(FR) 9 min read')
+        cy.get('#section-0-card-0')
+            .should('have.text', language ? 'Main sources of retirement income' : '(FR) Main sources of retirement income')
+        cy.get(':nth-child(2) > .grid > :nth-child(1) > .MuiButtonBase-root > .MuiCardContent-root > .m-0')
+            .should('have.text', language ? 'You can choose when to start your old age benefits. Understand what to consider in making that decision.' :
+                '(FR) You can choose when to start your old age benefits. Understand what to consider in making that decision.')
     })
 
     it('Learn overview - card 2 content', () => {
-        //cy.get('#english-button').click()
         let language = new utility().getLanguageTabletOrMonitorScreen()
-        cy.get(':nth-child(2) > .grid > :nth-child(2) > .no-underline > .p-6 > .text-xs').should('have.text',language ? '5 MIN READ' : '(FR) 5 MIN READ')
-        cy.get('#section-0-card-1').should('have.text',language ? 'Planning to save for retirement' : '(FR) Planning to save for retirement')
-        cy.get(':nth-child(2) > .grid > :nth-child(2) > .no-underline > .p-6 > .text-base').should('have.text', language ? 'Learn how to maximize your savings and to better manage your money in retirement.' : '(FR) Learn how to maximize your savings and to better manage your money in retirement.')
+        cy.get(':nth-child(2) > .grid > :nth-child(2) > .MuiButtonBase-root > .MuiCardContent-root > .text-sm')
+            .should('have.text', language ? '7 min read' : '(FR) 7 min read')
+        cy.get('#section-1-card-1')
+            .should('have.text', language ? 'From work to retirement' : '(FR) From work to retirement')
+        cy.get(':nth-child(1) > .grid > :nth-child(2) > .MuiButtonBase-root > .MuiCardContent-root > .m-0')
+            .should('have.text', language ? 'Explore how to maximize your savings and better manage your money in retirement.' :
+                '(FR) Explore how to maximize your savings and better manage your money in retirement.')
     })
 
     it('Learn overview - card 1 link click url redirects to /en/fr/ when accessing /en/fr', () => {
-        //cy.get('#english-button').click()
         let language = new utility().getLanguageTabletOrMonitorScreen()
         cy.wait(2000)
         cy.get('#section-0-card-0').click()
@@ -39,7 +42,6 @@ describe('test id 163 - verify Learn overview - Explore our Content - Manage Mon
     })
 
     it('Learn overview - card 2 link click url redirects to /en/fr/ when accessing /en/fr', () => {
-        //cy.get('#english-button').click()
         let language = new utility().getLanguageTabletOrMonitorScreen()
         cy.wait(2000)
         cy.get('#section-0-card-1').click()
