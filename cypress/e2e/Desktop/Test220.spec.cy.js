0cd9285a6cbc4643b9d2638452606fd7f328ba22
edmondhetu <edmondhetu88@gmail.com>
2023-04-19 14:42:29 -0400

    fix test 220

--- cypress/e2e/Desktop/Test220.spec.cy.js
+++ cypress/e2e/Desktop/Test220.spec.cy.js
@@ -7,13 +7,14 @@ describe('test id 220 - verify Plan link - Landing page', () => {
   it('plan text content', () => {
     cy.get('#english-button').click()
     let language = new utility().getLanguageTabletOrMonitorScreen()
-    cy.get(':nth-child(1) > .max-h-0 > :nth-child(1) > .flex-col > .rounded > .no-underline > .h1')
-      .should('have.text', language ? 'Learn ' : 'Learn (FR) ')
+    cy.get('.z-20 > :nth-child(2)')
+      .should('have.text', language ? 'Plan' : 'Plan (FR)')
   })
 
   it('plan content', () => {
     cy.get('#english-button').click()
     let language = new utility().getLanguageTabletOrMonitorScreen()
+    cy.get('.z-20 > :nth-child(2)').click()
     cy.get(':nth-child(1) > .max-h-0 > :nth-child(1) > .flex-col > .px-10 > .justify-center > :nth-child(1)')
       .should('have.text', language ? 'Learn about the "three pillars" of Canada\'s retirement system.' : '(FR) Learn about the "three pillars" of Canada\'s retirement system.')
     cy.get(':nth-child(1) > .max-h-0 > :nth-child(1) > .flex-col > .px-10 > .justify-center > :nth-child(2)')
@@ -25,7 +26,7 @@ describe('test id 220 - verify Plan link - Landing page', () => {
     let language = new utility().getLanguageTabletOrMonitorScreen()
     cy.wait(2000)
     cy.get('.z-20 > :nth-child(2)').click()
-    cy.get('.max-h-full > :nth-child(1) > .flex-col > .rounded > .no-underline > .h1').click()
+    cy.get('.max-h-full > :nth-child(1) > .flex > .rounded > .h1 > .MuiTypography-root > span').click()
     cy.location('pathname').should('equal', language ? '/en/plan' : '/fr/plan')
   })
 })
