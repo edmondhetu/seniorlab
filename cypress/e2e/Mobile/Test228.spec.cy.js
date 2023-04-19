ba7a6c08439d906892aae1fa2795112f70dbf20f
edmondhetu <edmondhetu88@gmail.com>
2023-04-19 15:06:23 -0400

    fix test 228

--- cypress/e2e/Mobile/Test228.spec.cy.js
+++ cypress/e2e/Mobile/Test228.spec.cy.js
@@ -11,10 +11,10 @@ describe('Test id 228 - Top Section - Mobile', () => {
         it('Friends and Family link title and paragraph content are visible', () => {
             cy.get('#english-button').click()
             let language = new utility().getLanguageMobileScreen()
-            cy.get('.pl-12 > .h2')
+            cy.get('.w-full > .mb-4')
                 .should('have.text', language ? 'Learn and plan for your retirement' : '(FR) Learn and plan for your retirement')
                 .and('be.visible')
-            cy.get('.pl-12 > .pb-2')
+            cy.get('.w-full > .m-0')
                 .should('have.text', language ? 'It\'s never too early or too late to learn about your retirement options and plan for your future. Find out about public pensions, ways to save your money and tips to consider for your retirement income.' :
                     '(FR) It’s never too early or too late to learn about your retirement options and plan for your future. Find out about public pensions, ways to save your money and tips to consider for your retirement income.')
                 .and('be.visible')
