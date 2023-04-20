8f76b05513eccd2a7086fb2a37b1172ce2916b86
Edmond Jr. Hetu <edmond.hetu@hrsdc-rhdcc.gc.ca>
2023-04-20 13:48:00 -0400

    fix text 133 155 to remove content

--- cypress/e2e/Desktop/Test133.spec.cy.js
+++ cypress/e2e/Desktop/Test133.spec.cy.js
@@ -4,28 +4,32 @@ describe('test id 133 - verify Button/Links - Landing page top section', () => {
   beforeEach(() => {
     cy.visit('/')
   })
-  it('language link', () => {
+  it('language link - verify is visible, clickable, html attributes and url pathname.', () => {
     cy.get('#english-button').click()
+    
     cy.get('[data-cy="toggle-language-link"]').click()
-    cy.get('.h4 > .font-body')
-      .should('have.text', 'Seniors Digital Gateway')
+    cy.get('[data-cy="toggle-language-link"]').should('have.attr', 'lang', 'en')
+      .and('have.attr', 'href', '/en/home')
+    cy.get('.h4 > .font-body').should('be.visible')
+    cy.location('pathname').should('equal', '/fr/home')
 
     cy.get('[data-cy="toggle-language-link"]').click()
-    cy.get('.h4 > .font-body')
-      .should('have.text', 'Seniors Digital Gateway (FR)')
+    cy.get('[data-cy="toggle-language-link"]').should('have.attr', 'lang', 'fr')
+      .and('have.attr', 'href', '/fr/home')
+    cy.get('.h4 > .font-body').should('be.visible')
+    cy.location('pathname').should('equal', '/en/home')
   })
 
-  it('checklist button label', () => {
+  it('checklist button - verify is visible, clickable, html attributes and url pathname', () => {
     cy.get('#english-button').click()
-    let language = new utility().getLanguageTabletOrMonitorScreen()
-    cy.get('.space-x-3 > :nth-child(1)')
-      .should('have.text', language ? 'Checklist' : 'Checklist')
-  })
 
-  it('mynotes button label', () => {
-    cy.get('#english-button').click()
     let language = new utility().getLanguageTabletOrMonitorScreen()
-    cy.get('.space-x-3 > :nth-child(2)')
-      .should('have.text', language ? 'My Notes' : 'My Notes')
+    cy.get('.space-x-3 > .text-white').should('have.attr', 'href', language ? '/en/home#' : '/fr/home#').and('be.visible')
+    cy.get('.space-x-3 > .text-white').click()
+    cy.location('pathname').should('equal', language ? '/en/home' : '/fr/home')
   })
 })
+
+
+
+
