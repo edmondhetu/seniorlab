b2e31b81d7fcce48b199828f7c6549efdaed7287
Edmond Jr. Hetu <edmond.hetu@hrsdc-rhdcc.gc.ca>
2023-04-20 14:52:25 -0400

    fix assert for verify language is visible

--- cypress/e2e/Desktop/Test133.spec.cy.js
+++ cypress/e2e/Desktop/Test133.spec.cy.js
@@ -6,17 +6,17 @@ describe('test id 133 - verify Button/Links - Landing page top section', () => {
   })
   it('language link - verify is visible, clickable, html attributes and url pathname.', () => {
     cy.get('#english-button').click()
-    
+
     cy.get('[data-cy="toggle-language-link"]').click()
     cy.get('[data-cy="toggle-language-link"]').should('have.attr', 'lang', 'en')
       .and('have.attr', 'href', '/en/home')
-    cy.get('.h4 > .font-body').should('be.visible')
+      .and('be.visible')
     cy.location('pathname').should('equal', '/fr/home')
 
     cy.get('[data-cy="toggle-language-link"]').click()
     cy.get('[data-cy="toggle-language-link"]').should('have.attr', 'lang', 'fr')
       .and('have.attr', 'href', '/fr/home')
-    cy.get('.h4 > .font-body').should('be.visible')
+      .and('be.visible')
     cy.location('pathname').should('equal', '/en/home')
   })
 
