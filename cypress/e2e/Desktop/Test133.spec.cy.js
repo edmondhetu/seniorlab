af1740c9577288a1d927b4a6c0a9500dcaca6155
Edmond Jr. Hetu <edmond.hetu@hrsdc-rhdcc.gc.ca>
2023-04-21 07:59:52 -0400

    run test133

--- cypress/e2e/Desktop/Test133.spec.cy.js
+++ cypress/e2e/Desktop/Test133.spec.cy.js
@@ -2,32 +2,31 @@ import { utility } from "../../support/Utility"
 
 describe('test id 133 - verify Button/Links - Landing page top section', () => {
   beforeEach(() => {
-    cy.visit('/')
+    cy.visit('/en/home')
   })
   it('language link - verify is visible, clickable, html attributes and url pathname.', () => {
-    cy.get('#english-button').click()
+    cy.get('[data-cy="toggle-language-link"]').click()
+    cy.get('[data-cy="toggle-language-link"]').should('have.attr', 'lang', 'fr')
+      .and('have.attr', 'href', '/fr/home')
+      .and('be.visible')
+    cy.location('pathname').should('equal', '/fr/home')
+    cy.contains('Retirement Hub (FR)')
 
     cy.get('[data-cy="toggle-language-link"]').click()
     cy.get('[data-cy="toggle-language-link"]').should('have.attr', 'lang', 'en')
       .and('have.attr', 'href', '/en/home')
       .and('be.visible')
-    cy.location('pathname').should('equal', '/fr/home')
-
-    cy.get('[data-cy="toggle-language-link"]').click()
-    cy.get('[data-cy="toggle-language-link"]').should('have.attr', 'lang', 'fr')
-      .and('have.attr', 'href', '/fr/home')
-      .and('be.visible')
     cy.location('pathname').should('equal', '/en/home')
+    cy.contains('Retirement Hub')
   })
 
   it('checklist button - verify is visible, clickable, html attributes and url pathname', () => {
-    cy.get('#english-button').click()
-
     let language = new utility().getLanguageTabletOrMonitorScreen()
-    cy.get('.space-x-3 > .text-white').should('have.attr', 'href', language ? '/en/home#' : '/fr/home#')
+
+    cy.get('.p-4 > .mb-6 > .MuiButtonBase-root').should('have.attr', 'href', language ? '/en/home#' : '/fr/home#')
       .and('be.visible')
-    cy.get('.space-x-3 > .text-white').click()
     cy.location('pathname').should('equal', language ? '/en/home' : '/fr/home')
+    cy.get('.p-4 > .mb-6 > .MuiButtonBase-root').click()
   })
 })
 
