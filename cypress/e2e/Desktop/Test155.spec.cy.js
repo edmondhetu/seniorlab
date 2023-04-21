f21388ce45342cb4cf9b9ac6740185abbfd28db7
Edmond Jr. Hetu <edmond.hetu@hrsdc-rhdcc.gc.ca>
2023-04-21 08:13:39 -0400

    run test 155

--- cypress/e2e/Desktop/Test155.spec.cy.js
+++ cypress/e2e/Desktop/Test155.spec.cy.js
@@ -1,29 +1,27 @@
 import { utility } from "../../support/Utility"
 
-describe('test id 155 - verify Learn link - Landing page', () => {
+describe('test id 155 - verify Learn tab - Landing page', () => {
   beforeEach(() => {
-    cy.visit('/')
+    cy.visit('/en/home')
   })
   it('learn tab - verify is visible, clickable and html attributes', () => {
-    cy.get('#english-button').click()
     cy.get('button').eq(0).should('have.attr', 'type', 'button')
-    .and('have.attr', 'tabindex', '0')
-    .and('have.attr', 'role', 'tab')
-    .and('have.attr', 'aria-selected', 'true')
-    .and('be.visible')
+      .and('have.attr', 'tabindex', '0')
+      .and('have.attr', 'role', 'tab')
+      .and('have.attr', 'aria-selected', 'true')
+      .and('be.visible')
     cy.get('button').eq(0).click()
   })
 
-  it('learn text is visible', () => {
-    cy.get('#english-button').click()
+  it('learn text title is visible', () => {
     cy.get('.h5').should('be.visible')
-    cy.get('.justify-center > :nth-child(1)').should('be.visible')
-    cy.get('.justify-center > :nth-child(2)').should('be.visible')
   })
 
-  it('learn link has href attribute /en/fr/ when accessing /en/fr', () => {
-    cy.get('#english-button').click()
+  it('explore retirement link - verify is visible, clickable and html attributes', () => {
     let language = new utility().getLanguageTabletOrMonitorScreen()
-    cy.get('.flex-row-reverse > .MuiTypography-root').should('have.attr', 'href', language ? '/en/home#' : '/en/home#')
+    cy.get('.text-right > .MuiButtonBase-root').should('have.attr', 'href', language ? '/en/learn' : '/fr/learn')
+      .and('be.visible')
+    cy.get('.text-right > .MuiButtonBase-root').click()
+    cy.location('pathname').should('equal', language ? '/en/home' : '/fr/home')
   })
 })
