80657fd59cba67fbee93830584b32e5f10dbc5fb
Edmond Jr. Hetu <edmond.hetu@hrsdc-rhdcc.gc.ca>
2023-04-28 18:24:45 -0400

    test insert spy on dom api

--- cypress/e2e/Desktop/Test319.spec.cy.js
+++ cypress/e2e/Desktop/Test319.spec.cy.js
@@ -1,26 +1,14 @@
 import { utility } from "../../support/Utility"
 
-describe('test id 220 - verify Plan link - Landing page', () => {
+describe('test id 319 - verify Top Learning links - Landing page', () => {
   beforeEach(() => {
-    cy.visit('en/home', {
+    cy.visit('/en/home', {
       onBeforeLoad: spyOnAddEventListener
     }).then({ timeout: 10000 }, waitForAppStart)
   })
-  it('plan tab - verify is visible and html attributes', () => {
-    cy.get('button').eq(1).should('have.attr', 'type', 'button')
-      .and('have.attr', 'tabindex', '-1')
-      .and('have.attr', 'role', 'tab')
-      .and('have.attr', 'aria-selected', 'false')
-      .and('be.visible')
-  })
 
-  it.only('plan link click url redirects to /en/fr/ when accessing /en/fr', () => {
-    let language = new utility().getLanguageTabletOrMonitorScreen()
-    cy.wait(2000)
-    cy.get('button').eq(1).click()
-    cy.get('.text-right > .MuiButtonBase-root').click()
-    cy.wait(2000)
-    cy.location('pathname').should('equal', language ? '/en/home' : '/fr/home')
+  it('Top learning title is visible', () => {
+    cy.get('h3').eq(2).should('be.visible')
   })
 })
 
