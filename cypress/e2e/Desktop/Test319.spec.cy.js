89110ca7ca0866ea452149e861f6097327364181
Edmond Jr. Hetu <edmond.hetu@hrsdc-rhdcc.gc.ca>
2023-05-01 14:24:50 -0400

    insert test 319

--- cypress/e2e/Desktop/Test319.spec.cy.js
+++ cypress/e2e/Desktop/Test319.spec.cy.js
@@ -1,14 +1,55 @@
 import { utility } from "../../support/Utility"
 
-describe('test id 319 - verify Top Learning links - Landing page', () => {
+describe('test id 319 - Main Sources of retirement income - Learning page', () => {
   beforeEach(() => {
-    cy.visit('/en/home', {
+    cy.visit('/en/learn/retirement-income-sources#overview', {
       onBeforeLoad: spyOnAddEventListener
     }).then({ timeout: 10000 }, waitForAppStart)
   })
 
-  it('Top learning title is visible', () => {
-    cy.get('h3').eq(2).should('be.visible')
+  it('left menu items are hidden \'Overview of retirement income sources\'', () => {
+    for (var i = 0; i < 6; i++) {
+      cy.get('li.MuiListItem-root.MuiListItem-gutters.mui-style-pjl2lh').eq(i)
+        .should('not.be.visible')
+    }
+  })
+
+  it('left menu item 1 \'Overview\'', () => {
+    let language = new utility().getLanguage()
+    cy.wait(2000)
+    cy.get('li.MuiListItem-root.MuiListItem-gutters.mui-style-pjl2lh').eq(0).click({ force: true })
+    cy.location('pathname').should('equal', language ? '/en/learn/retirement-income-sources' : '/fr/learn/retirement-income-sources')
+  })
+
+  it('left menu item 2 \'Canada\'s retirement income system\'', () => {
+    let language = new utility().getLanguage()
+    cy.wait(2000)
+    cy.get('li.MuiListItem-root.MuiListItem-gutters.mui-style-pjl2lh').eq(1).click({ force: true })
+    cy.location('pathname').should('equal', language ? '/en/learn/retirement-income-sources' : '/fr/learn/retirement-income-sources')
+  })
+
+  it('left menu item 3 \'Old Age Security (OAS) program\'', () => {
+    let language = new utility().getLanguage()
+    cy.get('li.MuiListItem-root.MuiListItem-gutters.mui-style-pjl2lh').eq(2).click({ force: true })
+    cy.location('pathname').should('equal', language ? '/en/learn/retirement-income-sources' : '/fr/learn/retirement-income-sources')
+  })
+
+  it('left menu item 4 \'Canada Pension Plan (CPP) program\'', () => {
+    let language = new utility().getLanguage()
+    cy.get('li.MuiListItem-root.MuiListItem-gutters.mui-style-pjl2lh').eq(3).click({ force: true })
+    cy.location('pathname').should('equal', language ? '/en/learn/retirement-income-sources' : '/fr/learn/retirement-income-sources')
+  })
+
+  it('left menu item 5 \'Ongoing earnings from your job\'', () => {
+    let language = new utility().getLanguage()
+    cy.get('li.MuiListItem-root.MuiListItem-gutters.mui-style-pjl2lh').eq(4).click({ force: true })
+    cy.location('pathname').should('equal', language ? '/en/learn/retirement-income-sources' : '/fr/learn/retirement-income-sources')
+  })
+
+  it('left menu item 6 \'Personal retirement savings\'', () => {
+    let language = new utility().getLanguage()
+    cy.get('li.MuiListItem-root.MuiListItem-gutters.mui-style-pjl2lh').eq(5).click({ force: true })
+    cy.location('pathname').should('equal', language ? '/en/learn/retirement-income-sources' : '/fr/learn/retirement-income-sources')
   })
 })
 
