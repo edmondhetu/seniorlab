4d253c12a692a5d240b3f3f625985563ed9ddaee
edmondhetu <128153188+edmondhetu@users.noreply.github.com>
2023-05-11 12:46:51 -0400

    test to click age pre-60 quiz

--- cypress/e2e/Desktop/QuizFlow.spec.cy.js
+++ cypress/e2e/Desktop/QuizFlow.spec.cy.js
@@ -1,10 +1,13 @@
 describe('test quiz age pre-60', () => {
-    it('test', () => {
-        cy.visit('/en/learn')
-        cy.get('#quiz-dialog-trigger').click()
-        cy.get('h2')
-            .should('be.visible')
-        cy.get('.MuiDialogActions-root > .MuiButtonBase-root').click()
+    const age = ["'[value=\"pre-60\"]'"]
+    age.forEach(age => {
+        it('test', () => {
+            cy.visit('/en/learn')
+            cy.get('#quiz-dialog-trigger').click()
+            cy.get('h2')
+                .should('be.visible')
+            cy.get('.MuiDialogActions-root > .MuiButtonBase-root').click()
+            cy.get(age).click()
+        })
     })
 })
-
